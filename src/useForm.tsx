d2624c66aba71b0f5c5405367e0b9dbcd1728e02
kingdaro <kingdaro@gmail.com>
2019-08-11 02:39:53 -0500

    more type cleanup

--- src/useForm.tsx
+++ src/useForm.tsx
@@ -4,8 +4,12 @@ type FieldMap = {
   }
 }
 
+type FieldValues<Fields extends FieldMap> = {
+  [K in keyof Fields]: Fields[K]["value"]
+}
+
 function useForm<Fields extends FieldMap>(fields: Fields) {
-  const values = {} as { [K in keyof Fields]: Fields[K]["value"] }
+  const values = {} as FieldValues<Fields>
   for (const inputName in fields) {
     values[inputName] = fields[inputName].value
   }
